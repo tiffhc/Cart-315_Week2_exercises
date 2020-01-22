80560a52fbdff3eb3e574a37db76311b6156a6bf
tiffhc <46658494+tiffhc@users.noreply.github.com>
2020-01-22 12:29:05 -0500

    Week 3 changes

---
+++ Assets/Rotate.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//default import 
+
+ //public class is accessable. 
+public class Rotate : MonoBehaviour //rotate is a subclass of MonoBehaviour (class of all components) a lot of things we don't need to do
+{
+    public float rotate_speed = 0.1f; 
+
+    // Start is called before the first frame update
+    //void do stuff without returnning anything, unity will run this function 
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //eulers - x, y, and z
+        if (Input.GetButton("Fire1"))
+        {
+            this.transform.Rotate(0.0f, rotate_speed, 0.0f);
+        }
+
+    }
+}
---
+++ Assets/bounce.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//when player enters the zone, make them bounce
+
+public class bounce : MonoBehaviour
+{
+    public GameObject the_other_object; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        //this.GetComponent<Rotate> - dont add a component that is not there. Missing rotate script. 
+        if(other.gameObject == the_other_object)
+        {
+            other.GetComponent<Rigidbody>().AddForce(0, 500.0f * Time.deltaTime, 0);
+        }
+        //every update that the object is inside the trigger zone, it adds an upward force
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == the_other_object) //refer to the game object of the collider, now only the object with audio is affected
+        {
+            other.GetComponent<AudioSource>().Play();
+        }
+        
+    }
+    //on trigger entering, Unity will look for on anything that has a trigger. 
+    //private void OnTriggerEnter(Collider other) //other is what is being collided with it?
+    // {
+    //how to access rigid body component
+    //other.GetComponent<Rigidbody>().AddForce(0, 700.0f, 0); //call the get component method on rigid body of that component
+    // }
+}
